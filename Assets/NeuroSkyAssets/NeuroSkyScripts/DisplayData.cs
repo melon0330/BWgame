5e1a147db82790374b1cebb2988a98d9de6e9fd1
masterLee1234 <lhjin0j@naver.com>
2021-12-26 03:51:15 +0900

    updated by jin

--- Assets/NeuroSkyAssets/NeuroSkyScripts/DisplayData.cs
+++ Assets/NeuroSkyAssets/NeuroSkyScripts/DisplayData.cs
@@ -12,8 +12,12 @@ public class DisplayData : MonoBehaviour
     private int poorSignal1;
     private int attention1;
     private int meditation1;
-	
+	private int blink;
+
 	private float delta;
+
+	//여기서 부터 수정사항임
+	private GameObject target;
 
     void Start()
     {
@@ -23,7 +27,8 @@ public class DisplayData : MonoBehaviour
 		controller.UpdatePoorSignalEvent += OnUpdatePoorSignal;
 		controller.UpdateAttentionEvent += OnUpdateAttention;
 		controller.UpdateMeditationEvent += OnUpdateMeditation;
-		
+		controller.UpdateBlinkEvent += OnUpdateBlink;
+
 		controller.UpdateDeltaEvent += OnUpdateDelta;
 		
     }
@@ -44,16 +49,40 @@ public class DisplayData : MonoBehaviour
 	}
 	void OnUpdateAttention(int value){
 		attention1 = value;
+		if(attention1 >= 50 && meditation1 < 50)
+        {
+			//타깃에게 메세지를 보낼거임 (함수이름, 인자)
+			target.SendMessage("attention", 1000);
+        } 
+		else if(attention1 >= 50 && meditation1 >= 50)
+        {
+			target.SendMessage("attention", 1002);
+
+		}
 	}
 	void OnUpdateMeditation(int value){
 		meditation1 = value;
+		if (meditation1 >= 50 && attention1 < 50)
+		{
+			//타깃에게 메세지를 보낼거임 (함수이름, 인자)
+			target.SendMessage("attention", 1001);
+		}
+		else if(attention1 < 50 && meditation1 < 50)
+		{
+			target.SendMessage("attention", 1003);
+
+		}
 	}
 	void OnUpdateDelta(float value){
 		delta = value;
 	}
+	void OnUpdateBlink(int value)
+	{
+		blink = value;
+		//물건 선택 코드 넣어서 target 에 지정해주면 됨. 근데 이게 눈을 깜박이는 세기에 따라서 값이 달라서 이거를 이용할수 있을것 같음 대략 40부터 120사이정도로 나눌수 있음
+	}
 
-
-    void OnGUI()
+	void OnGUI()
     {
 		GUILayout.BeginHorizontal();
 		
@@ -67,7 +96,7 @@ public class DisplayData : MonoBehaviour
             controller.Disconnect();
 			indexSignalIcons = 1;
         }
-		
+
 		GUILayout.Space(Screen.width-250);
 		GUILayout.Label(signalIcons[indexSignalIcons]);
 		
@@ -78,6 +107,7 @@ public class DisplayData : MonoBehaviour
         GUILayout.Label("Attention1:" + attention1);
         GUILayout.Label("Meditation1:" + meditation1);
 		GUILayout.Label("Delta:" + delta);
+		GUILayout.Label("Blink:" + blink);
 
-    }
+	}
 }
